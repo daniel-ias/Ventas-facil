ef1b8d74fab3ebd789488d6e8d11c45652829c18
DanielCuervo <daniel.cuervo@ias.com.co>
2023-10-02 08:53:01 -0500

    "Added application.yml and delete controller not necessary"

--- src/main/java/com/ecommerce/core/controller/ArticulosController.java
+++ src/main/java/com/ecommerce/core/controller/ArticulosController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/catalogo")
+@CrossOrigin(origins = "*")
 public class ArticulosController {
     private ArticuloService articuloService;
 
